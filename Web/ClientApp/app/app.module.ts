d2504816ccf02149c201c29a238db0e225236042
Saidu Manohar <manohar.saidu@gmail.com>
2017-08-24 15:58:02 +0530

    Setting Components

--- Web/ClientApp/app/app.module.ts
+++ Web/ClientApp/app/app.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
-import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/shared/Header/header.Component'
-import { CourseSearchComponent } from './components/Course/courseSearch.Component'
-import { PersonSearchComponent } from './components/Person/personSearch.Component'
-import { LoginComponent } from './components/Login/login.Component';
-import { CourseService } from './components/shared/courseService';
 import { FormsModule } from '@angular/forms';
  
 
@@ -15,11 +10,7 @@ import { FormsModule } from '@angular/forms';
     bootstrap: [AppComponent],
     declarations: [
         AppComponent,
-        HomeComponent,
         HeaderComponent,
-        CourseSearchComponent,
-        PersonSearchComponent,
-        LoginComponent,
         
     ],
     imports: [
@@ -27,14 +18,9 @@ import { FormsModule } from '@angular/forms';
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'login', component: LoginComponent },
-            { path: 'course', component: CourseSearchComponent},
-            { path: 'personDetails/:id', component: PersonSearchComponent},
             { path: '**', redirectTo: 'home' }
         ])
     ],
-    providers: [CourseService]
 })
 export class AppModule {
 }
