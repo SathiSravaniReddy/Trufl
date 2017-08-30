f0e602d9ecd03f45999f3e719386e44b220e9f15
Sunil Konda <sunil.konda@ptgindia.com>
2017-08-30 11:05:10 +0530

    webapi getuser list commit

---
+++ Trufl.Services/Controllers/TruflController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DTO;
+using Trufl.Data_Access_Layer;
+using Trufl.Business_Layer;
+using System.Data;
+
+namespace Trufl.Services.Controllers
+{
+    [RoutePrefix("api/Trufl")]
+    public class TruflController : ApiController
+    {
+        AdminBL _adminBL = new AdminBL();
+        AdminDL _adminDL = new AdminDL();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetTruflUserList")]
+        [HttpGet]
+        public List<UserProfile> GetTruflUserList()
+        {
+            return _adminBL.RetrieveUser();
+        }
+
+        [Route("SaveTruflUserInfromation")]
+        [HttpPost]
+        public bool SaveTruflUserInfromation(List<TruflUserInputDTO> truflUserInputDTO)
+        {
+            return _adminBL.SaveTruflUserInfromation(truflUserInputDTO);
+        }
+
+       
+        
+    }
+}
