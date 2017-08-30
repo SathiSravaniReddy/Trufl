03b3c89df29b2e53998e8f203cab89f2b31b387b
shyamptg <shyamsundar.thirri@ptgindia.com>
2017-08-29 23:05:28 -0700

    Hostess Seated UI page integrated

--- Web/ClientApp/app/app.module.ts
+++ Web/ClientApp/app/app.module.ts
@@ -4,20 +4,31 @@ import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component';
 import { HostessComponent } from './components/Hostess/hostess.component';
 import { FormsModule } from '@angular/forms';
+import { SeatedComponent } from './components/seated/seated.component';
+import { SeatedService } from './components/seated/seated.service';
+
+
 
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [
         AppComponent,
-        HostessComponent
+        HostessComponent,
+        SeatedComponent
     ],
+    providers: [
+        SeatedService
+    ],
+
     imports: [
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        UniversalModule, // Must be firsst import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: '**', redirectTo: 'home' }
+            { path: '', component: HostessComponent },    
+            { path: 'getseatedpage', component: SeatedComponent },
+            { path: 'home', component: HostessComponent },
+            { path: '**', redirectTo: 'home' },
         ])
     ],
 })
