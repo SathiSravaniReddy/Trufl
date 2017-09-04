3a98838d45c7c282cc6f023c0ec0fe108b0e8807
shyamptg <shyamsundar.thirri@ptgindia.com>
2017-09-04 04:19:28 -0700

    changes in seated page

--- Web/ClientApp/app/components/seated/seated.service.ts
+++ Web/ClientApp/app/components/seated/seated.service.ts
@@ -5,13 +5,29 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class SeatedService {
     private results: any;
+    public seatedInfo: any;
+    public data: any;
+    public seatsdetails: any;
+
     constructor(private http: Http) {
     }
 
     getSeatedDetails() {
         console.log("into service");
-        return this.http.get('assets/seated.json').map(
+        return this.http.get('http://localhost:8679/api/Trufl/GetSeatedUsersList/1').map(
             (res) => res.json())
 
     }
+
+    postSeatedDetails(seatedInfo: any) {
+        this.seatsdetails = seatedInfo;
+        console.log(this.data);
+        return this.http.post('http://localhost:8679/api/Trufl/SaveSeatBookingUsersList', this.seatsdetails).map(
+            (res) => res.json()
+        )
+    } 
+
+
+   
+
 }
