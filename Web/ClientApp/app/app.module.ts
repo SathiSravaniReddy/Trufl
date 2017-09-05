e853915393fe80c9cd32d4c2edb51a9ea5172d60
Nirupama Bommireddy <nirupamabommireddy@outlook.com>
2017-09-05 15:04:01 +0530

    changes to app.module.ts

--- Web/ClientApp/app/app.module.ts
+++ Web/ClientApp/app/app.module.ts
@@ -17,7 +17,7 @@ import { HeaderComponent } from './components/shared/Header/header.Component';
 import { HostessService } from './components/Hostess/hostess.service';
 import { LoginService } from './components/shared/login.service';
 import { TimerComponent } from './components/Hostess/timer.component';
-
+import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common'
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -37,7 +37,8 @@ import { TimerComponent } from './components/Hostess/timer.component';
     providers: [
         SeatedService,
         HostessService,
-        LoginService
+        LoginService,
+        [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }]
     ],
 
     imports: [
@@ -46,7 +47,7 @@ import { TimerComponent } from './components/Hostess/timer.component';
         CommonModule,
         RouterModule.forRoot([
             { path: '', component: LoginComponent },    
-            { path: 'getseatedpage', component: SeatedComponent },
+            { path: 'seated', component: SeatedComponent },
             { path: 'home', component: HostessComponent },
             { path: 'login', component: LoginComponent },
             { path: 'register', component: RegisterComponent },
