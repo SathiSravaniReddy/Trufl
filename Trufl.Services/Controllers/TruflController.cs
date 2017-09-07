355c9ccd3bdebace892ccab51c4b1e3ca4444a38
Sunil Konda <sunil.konda@ptgindia.com>
2017-09-07 11:31:46 +0530

    booking seated SaveWaitedlistBooking webapi done.

--- Trufl.Services/Controllers/TruflController.cs
+++ Trufl.Services/Controllers/TruflController.cs
@@ -94,6 +94,21 @@ namespace Trufl.Services.Controllers
             }
         }
 
+        [Route("SaveWaitedlistBooking")]
+        [HttpPost]
+        public object SaveWaitedlistBooking(BookingTableInputDTO bookingTableInput)
+        {
+            bool res = _hostessBL.SaveWaitedlistBooking(bookingTableInput);
+            try
+            {
+                return Json(new JsonResponseResult { _ErrorCode = TruflConstants._ErrorCodeSuccess, _Data = res, _StatusCode = TruflConstants._StatusCodeOK, _StatusMessage = TruflConstants._StatusMessageSuccess });
+            }
+            catch (Exception ex)
+            {
+                return Json(new JsonResponseResult { _ErrorCode = TruflConstants._ErrorCodeFailed, _Data = ex.ToString(), _StatusCode = TruflConstants._StatusCodeFailed, _StatusMessage = TruflConstants._StatusMessageFailed });
+            }
+        }
+
         #endregion
 
         #region Seated User
