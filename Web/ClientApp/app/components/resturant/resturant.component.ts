24a006377fb6dabdf37f00525eb84d06fc2e14e0
shyamptg <shyamsundar.thirri@ptgindia.com>
2017-09-10 21:45:14 -0700

    restaurent commited

--- Web/ClientApp/app/components/resturant/resturant.component.ts
+++ Web/ClientApp/app/components/resturant/resturant.component.ts
@@ -1,5 +1,5 @@
 ﻿
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RestaurenService } from './restaurent.service';
 import { Pipe, PipeTransform } from '@angular/core';
 
@@ -13,40 +13,59 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 })
 export class ResturantComponent implements OnInit {
     public restaurent_info: any;
+    public notifications_info: any;
+    public states: any;
 
-    user: FormGroup;
-     name: FormControl; 
-
-
-     constructor(private restaurenService: RestaurenService) {
-
-        /*    this.myForm = new FormGroup({
-                    restaurentname: new FormControl(''),                
-                    emailid: new FormControl('', Validators.required),
-                    contact1: new FormControl('', Validators.required),
-                    contact2: new FormControl('', Validators.required),
-
-                    address1: new FormControl('', Validators.required),
-                    address2: new FormControl('', Validators.required),
-                    state: new FormControl('', Validators.required),
-                    zipcode: new FormControl('', Validators.required),
+    myForm: FormGroup;
+    //name: FormControl; 
 
 
-                    ownername: new FormControl('', Validators.required),
+    constructor(private restaurenService: RestaurenService, private fb: FormBuilder) {
 
-                    owneremail: new FormControl('', Validators.required),
-                    ownercontact1: new FormControl('', Validators.required),
-                    ownercontact2: new FormControl('', Validators.required),
-                    description: new FormControl('', Validators.required)
+     /*   this.myForm = new FormGroup({
+            restaurentname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+            emailid: new FormControl('', [Validators.required]),
+            contact1: new FormControl('', Validators.required),
+            contact2: new FormControl('', Validators.required),
+            address1: new FormControl('', Validators.required),
+            address2: new FormControl('', Validators.required),
+            state: new FormControl('', Validators.required),
+            zipcode: new FormControl('', Validators.required),
+            ownername: new FormControl('', Validators.required),
+            owneremail: new FormControl('', Validators.required),
+            ownercontact1: new FormControl('', Validators.required),
+            ownercontact2: new FormControl('', Validators.required),
+            description: new FormControl('', Validators.required)
 
-                    
-         });*/
+
+        }); */
+        this.myForm = fb.group({
+            'restaurentname': [null, Validators.required],
+            'emailid': [null, Validators.required],
+            'contact1': [null, Validators.required],
+            'contact2': [null, Validators.required],
+            'address1': [null, Validators.required],
+            'address2': [null, Validators.required]
+        })
+        
 
     }
     ngOnInit() {
 
         this.getrestaurent();
+        this.getnotifications();
+
+        this.states = [{ name: 'Telangane', value: 0 },
+                       { name: 'AP', value: 0 },
+                       { name: 'Kerala', value: 0 },
+                       { name: 'Tamilnadu', value: 0 },
+
+        ];
+
+        
     }
+
+
     public getrestaurent() {
         this.restaurenService.getRestaurentDetails().subscribe((res: any) => {
 
@@ -54,10 +73,30 @@ export class ResturantComponent implements OnInit {
             console.log(this.restaurent_info);
         })
 
-    }  
+    }
+
+    public getnotifications() {
+
+        this.restaurenService.getnotifications().subscribe((res: any) => {
+
+            this.notifications_info = res.data;
+            console.log(this.notifications_info);
+        })
+
+    }
+
+    onSubmit(details: any) {
+        this.restaurenService.addRestaurentDetails(details).subscribe((res: any) => {
+            console.log(res);
+
+        })
+
+       
+    }
+
 }
 @Pipe({
-    name:'searchPipe'
+    name: 'searchPipe'
 })
 export class SearchPipe implements PipeTransform {
 
