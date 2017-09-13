edd28d72d9883dde950bc8ea619024b647a5f6a5
shyamptg <shyamsundar.thirri@ptgindia.com>
2017-09-13 05:21:22 -0700

    toestars notification commited

--- Web/ClientApp/app/components/seated/seated.component.ts
+++ Web/ClientApp/app/components/seated/seated.component.ts
@@ -1,13 +1,17 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewContainerRef } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { SeatedService } from './seated.service';
 import { PaginationControlsComponent } from 'ngx-pagination';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
+
+import { ToastOptions } from 'ng2-toastr';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
     selector: 'seated',
     templateUrl: './seated.component.html',
-    styleUrls: ['./seated.component.css']
+    styleUrls: ['./seated.component.css'],
+    providers: [ToastsManager, ToastOptions]
 })
 export class SeatedComponent implements OnInit {
 
@@ -15,15 +19,15 @@ export class SeatedComponent implements OnInit {
     public isenabled = false;
     private seatedinformation: any;
     public items: any = [];
-    load: boolean = false;
-
-
-    // loading: boolean = false;
+    load: boolean = false;   
 
 
     private arr = ['Seated', 'AppServed', 'MenuServed', 'DesertServed', 'CheckReceived', 'Boozing', 'Empty'];
 
-    constructor(private seatedService: SeatedService,private router:Router) {
+    constructor(private seatedService: SeatedService, private router: Router, private _toastr: ToastsManager, vRef: ViewContainerRef) {
+
+        this._toastr.setRootViewContainerRef(vRef);
+        //called first time before the ngOnInit()
 
     }
 
@@ -41,21 +45,13 @@ export class SeatedComponent implements OnInit {
                         obj[keyName] = (obj[keyName] == 1) ? true : false;
                     }
                 })
-            });
+            });          
 
-
-            console.log(this.seatedinfo);
-
-            this.seatedinfo = this.seatedinfo.filter(function (obj) {
+           this.seatedinfo = this.seatedinfo.filter(function (obj) {
                 return !obj['Empty']
             })
 
-            //  console.log(this.seatedinfo);
-            /*.seatedinfo.map((item, index) => {
-                if (item.Empty == '1') {
-                    this.seatedinfo.splice(index,1)
-                }
-            })*/
+          
 
         })
 
@@ -67,10 +63,7 @@ export class SeatedComponent implements OnInit {
         { value: 0 },
         { value: 1 }
     ];
-
-    /* change() {
-         this.isenabled = true;
-     }*/
+   
     public get(data: any, type: any) {
 
         var details = {
@@ -94,78 +87,47 @@ export class SeatedComponent implements OnInit {
 
         } else {
             this.items.push(details);
-        }
-
-        console.log(this.items);
-        //if (event.target.checked) {
-        //    details.AmenitiChecked = true;
-
-        //    this.items.push(details);
-
-        //}
-        //else {
-
-        //    if (details.AmenitiChecked == 1) {
-        //        details.AmenitiChecked = false;
-        //        this.items.push(details);
-        //    }
-        //    else {
-        //        details.AmenitiChecked = false
-        //        this.items.map((item, index) => {
-        //            if (item.TruflUserID == data['TruflUserID'] && item.AmenitiName == type) {
-        //                this.items.splice(index, 1);
-        //            }
-        //        })
-        //    }
-
-
-        //}
+        }            
 
 
     }
 
 
-    postSeatedDetails() {
-        console.log(this.items);
+    postSeatedDetails() {      
 
-        //this.loading = true;
+        
         this.load = true;
 
         this.seatedService.postSeatedDetails(this.items).subscribe((res: any) => {
            
-            //  this.getSeatedDetails();
+            
             if (res['_StatusMessage'] =="Success") {               
-               /* setInterval(() => {                 
+                setInterval(() => {                 
                     this.load = false;                
                   
-                }, 1000); */ 
-                this.getvalue(function (value) {
-                   
-                });
+                }, 2000); 
+             
 
                 this.seatedinfo = [];
                 this.getSeatedDetails();
                
             }
 
-        })  
 
-      
+           
+            window.setTimeout(() => {
+                this._toastr.success("data saved successfully");
+            }, 200);
+           
+
+        })      
       
 
     }
-
-    getvalue(callback) {
-
-        setInterval(() => {
-            this.load = false;
-
-        }, 2000);
-
-    }
-    /*public hasData(): boolean {
+   
+    public hasData(): boolean {
         return (this.seatedinfo != null && this.seatedinfo.length > 0);
-    } */
+    } 
 
 
 
