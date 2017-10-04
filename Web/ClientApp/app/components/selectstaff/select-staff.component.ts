323cf49238c2625780f71b0d612a18c422b0347a
Saidu Manohar <manohar.saidu@gmail.com>
2017-10-04 11:50:51 +0530

    Starting Screens

--- Web/ClientApp/app/components/selectstaff/select-staff.component.ts
+++ Web/ClientApp/app/components/selectstaff/select-staff.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'selectStaff',
     templateUrl: './select-staff.component.html',
-   
+
 })
 export class SelectStaffComponent {
+    constructor(private router: Router) {
+
+    }
+    back() {
+        this.router.navigateByUrl('/selectselections');
+    }
+    next() {
+        this.router.navigateByUrl('/reviewSelections');
+    }
 }
