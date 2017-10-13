4b207254d67d84f5266e798b67c268716eabaae5
Sravani <sravani.sathi@ptgindia.com>
2017-10-13 18:01:52 +0530

    changes to defaultsettings,seated page,header page,snapshot page,styles

--- Web/ClientApp/app/components/shared/Header/header.Component.ts
+++ Web/ClientApp/app/components/shared/Header/header.Component.ts
@@ -6,92 +6,75 @@ import { Router, RouterLinkActive  } from '@angular/router';
 @Component({
     selector: 'shared-header',
     templateUrl: './header.Component.html',
-    styleUrls:['./header.component.css']
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
     private userType;
     private userName;
-    private profileVisible:boolean = false;
+    private profileVisible: boolean = false;
     private showHeadings = true;
     public isSettings = false;
     private showDashboard = true;
     private employeeVisible: boolean = false;
     public loadHeaders = {};
     public headers = [];
-  
-   
+
+
     constructor(private loginService: LoginService, private router: Router) {
         this.userType = this.loginService.getUserType();
         this.userName = this.loginService.getUserName();
         //Keep these load headers in a service-----
         this.loadHeaders = {
             "RA": [
-               
-            {
-                "name": "Waitlist",
-                "active": true,
-                "route": '/waitlist'
-            },
-            {
-                "name": "Seated",
-                "active": false,
-                "route": '/seated'
-            },
-            {
 
-                "name": "SnapShot",
-                "active": false,
-                "route": '/snapshot'
-            },
-            {
-               
-                "name": "Settings",
-                "active": true,
-                "route": '/defaultSettings'
-            }
-        ],
-           "TA": [
-               {
-                "name": "Dashboard",
-                "active": true,
-                "route": '/dashboard'
+                {
+                    "name": "WAITLIST",
+                    "active": true,
+                    "route": '/waitlist'
                 },
-               {
-                "name": "Restaurant",
-                "active": false,
-                "route": '/restaurant'
-               },
-               {
-                   "name": "Settings",
-                   "active": true,
-                   "route": '/settings'
-               }
-           ]
+                {
+                    "name": "SEATED",
+                    "active": false,
+                    "route": '/seated'
+                },
+                {
+
+                    "name": "SNAPSHOT",
+                    "active": false,
+                    "route": '/snapshot'
+                },
+                {
+
+                    "name": "SETTINGS",
+                    "active": false,
+                    "route": '/defaultSettings'
+                }
+            ],
+            "TA": [
+                {
+                    "name": "Dashboard",
+                    "active": true,
+                    "route": '/dashboard'
+                },
+                {
+                    "name": "Restaurant",
+                    "active": false,
+                    "route": '/restaurant'
+                },
+                {
+                    "name": "Settings",
+                    "active": true,
+                    "route": '/settings'
+                }
+            ]
 
         };
 
         this.headers = this.loadHeaders[this.userType];
 
-        if ((router.url === '/waitlist') || (router.url === '/seated')) {
+        if ((router.url === '/waitlist') || (router.url === '/seated') || (router.url === '/snapshot') || (router.url === '/defaultSettings')) {
             this.headers.map(function (obj, index) {
-                if ([0,1, 2].indexOf(index) >= 0) {
-                    obj.isShow = true;
-                } else {
-                    obj.isShow = false;
-                }
-            });
-            
-        }
-
-        if ((router.url === '/hostessdashboard') || (router.url === '/hostesssettings') || (router.url === '/settings')) {
-            this.headers.map(function (obj, index) {
-                obj.isShow = obj.route == router.url;
-            });
-        }
-        
-        if ((router.url === '/dashboard') || (router.url === '/restaurant')) {
-            this.headers.map(function (obj, index) {
-                if ([0, 1].indexOf(index) >= 0) {
+                if ([0, 1, 2, 3].indexOf(index) >= 0) {
                     obj.isShow = true;
                 } else {
                     obj.isShow = false;
@@ -99,34 +82,52 @@ export class HeaderComponent {
             });
 
         }
-       
 
-    }
- 
-    logoutShow() {
-        this.profileVisible = this.profileVisible == false?true:false;
-        if (this.userType == "RA") {
-            this.employeeVisible = true;
-        }
-       
-    }
+        //    if ((router.url === '/hostessdashboard') || (router.url === '/hostesssettings') || (router.url === '/defaultSettings')) {
+        //        this.headers.map(function (obj, index) {
+        //            obj.isShow = obj.route == router.url;
+        //        });
+        //    }
 
-    showHeaders() {
-        if (this.userType === "RA") {
-            this.router.navigateByUrl('/hostessdashboard');
-        }
-        else if (this.userType === "TA"){
-            this.router.navigateByUrl('/dashboard');
-        }
-    }
-    logout() {
-        this.loginService.logout();
-        this.router.navigateByUrl('/login');
-    }
+        //    if ((router.url === '/dashboard') || (router.url === '/restaurant')) {
+        //        this.headers.map(function (obj, index) {
+        //            if ([0, 1].indexOf(index) >= 0) {
+        //                obj.isShow = true;
+        //            } else {
+        //                obj.isShow = false;
+        //            }
+        //        });
 
-    getEmployee() {
+        //    }
 
 
-        this.router.navigate(['employeeconfiguration']);
+        //}
+
+        //logoutShow() {
+        //    this.profileVisible = this.profileVisible == false?true:false;
+        //    if (this.userType == "RA") {
+        //        this.employeeVisible = true;
+        //    }
+
+        //}
+
+        //showHeaders() {
+        //    if (this.userType === "RA") {
+        //        this.router.navigateByUrl('/hostessdashboard');
+        //    }
+        //    else if (this.userType === "TA"){
+        //        this.router.navigateByUrl('/dashboard');
+        //    }
+        //}
+        //logout() {
+        //    this.loginService.logout();
+        //    this.router.navigateByUrl('/login');
+        //}
+
+        //getEmployee() {
+
+
+        //    this.router.navigate(['employeeconfiguration']);
+        //}
     }
 }
